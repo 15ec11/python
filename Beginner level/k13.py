c7ebcc4e94300c7aec8d2def6383f656b90f2e53
15ec11 <gajapathy027@gmail.com>
2018-08-30 15:46:06 +0530

    Create k13.py

---
+++ Beginner level/k13.py
@@ -0,0 +1,13 @@
+x=int(input())
+y=int(input())
+z=[]
+s=[]
+for x in range(x):
+    z.append(int(input()))
+for y in range(y):
+    s.append(int(input()))
+for p in s:
+    if p in z:
+        print("YES")
+    else:
+        print("NO")
